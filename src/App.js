3d874e427357d860194d37e610af9a734082bf36
ANGIELOUX <angelajohnson943@gmail.com>
2022-06-10 02:20:46 +1000

    Configure useState hook to save user input as new Todo item

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 // Importing components
-import Form from './components/Form'
-import ToDoList from './components/ToDoList';
+import Form from "./components/Form";
+import ToDoList from "./components/ToDoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState("");
+
   return (
     <div className="App">
       <header>
         <h1>aj's to dos</h1>
       </header>
-      <Form/>
-      <ToDoList/>
+      <Form
+        setInputText={setInputText}
+        value={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+      />
+      <ToDoList />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,39 @@
-import React from 'react'
+import React from "react";
 
-const Form = () => {
-    return(
-        <form>
-            <input type="text" className="todo-input" />
-            <button className="todo-button" type='submit'>
-                <i className="fas fa-plus-square"></i>
-            </button>
-            <div className="select">
-                <select name="todos" className="filter-todo">
-                    <option value="all">All</option>
-                    <option value="complete">Done</option>
-                    <option value="incomplete">To do</option>
-                </select>
-            </div>
-        </form>
-    )
-}
+const Form = ({ setInputText, todos, setTodos, inputText }) => {
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const submitTodoHandler = (e) => {
+    // prevent the page from reloading on form submit
+    e.preventDefault();
+    
+    // add the new item to the todo array based on user input and give it a random id
+    setTodos([
+      ...todos,
+      { text: inputText, completed: false, id: Math.round(Math.random() * 10000) },
+    ]);
+
+    // reset the input back to nil once the to-do item has been added
+    setInputText('')
+  };
+
+  return (
+    <form>
+      <input onChange={inputTextHandler} type="text" className="todo-input" />
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
+        <i className="fas fa-plus-square"></i>
+      </button>
+      <div className="select">
+        <select name="todos" className="filter-todo">
+          <option value="all">All</option>
+          <option value="complete">Done</option>
+          <option value="incomplete">To do</option>
+        </select>
+      </div>
+    </form>
+  );
+};
 
 export default Form;
